d65c087b14a0185d243d63d4f62aa5b2a630d703
tdhieu159 <tdhieu1599@gmail.com>
2024-11-24 12:57:32 +0700

    update login

--- src/main/java/com/example/LaptopShop/LaptopShopApplication.java
+++ src/main/java/com/example/LaptopShop/LaptopShopApplication.java
@@ -3,7 +3,9 @@ package com.example.LaptopShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopShopApplication {
 
 	public static void main(String[] args) {
