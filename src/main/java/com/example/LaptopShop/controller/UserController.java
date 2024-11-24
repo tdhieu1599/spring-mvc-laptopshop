44cc75541c3bdc987c555644cf140ed87d9c79b8
tdhieu159 <tdhieu1599@gmail.com>
2024-11-25 00:27:52 +0700

    set up JSP 25.11.2024

---
+++ src/main/java/com/example/LaptopShop/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.LaptopShop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LaptopShop.service.UserService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "hello";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// @Autowired
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
---
+++ src/main/java/com/example/LaptopShop/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.LaptopShop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String handleHello() {
+        return "hello from service";
+    }
+}
